a14f771fe0cd1bce0a000bd3f81075cbc60cb10c
dnyandip <pawardnyandip01999@gmail.com>
2023-06-08 11:56:31 +0530

    delete

--- src/app/datatype.ts
+++ src/app/datatype.ts
@@ -1,16 +1,14 @@
-// export interface Element {
-//   User_Detail:string[];
-//   Location: string;
-//   Battery_Details: string[];
-//   Rating:string;
-//   id:number;
-// }
-
 export interface Element {
-  User_Detail:{Name:string,id:number};
-
+  User_Detail: {
+    Name: string;
+    id: number;
+  };
   Location: string;
-  Battery_Details: {Vehicle_Type:string, Battery_Percent:number,Battery_id:number};
-  Rating:string;
-  id:number;
+  Battery_Details: {
+    Vehicle_Type: string;
+    Battery_Percent: number;
+    Battery_id: string;
+  };
+  Rating: number;
+  id: number;
 }
